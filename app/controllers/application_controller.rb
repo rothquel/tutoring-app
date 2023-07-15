a121083cb0a1efe4113d3c041c3d26e735d3bd06
alchemy1 <nic.rothquel@alchemytuition.com.au>
2023-07-14 20:44:19 -1000

    login page, password reset and default dashboard page created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_tutor!
+
+  protected
+
+  def after_sign_in_path_for(tutor)
+    tutor_dashboard_path
+  end
 end
--- app/controllers/tutors/sessions_controller.rb
+++ app/controllers/tutors/sessions_controller.rb
@@ -1,17 +1,16 @@
 # frozen_string_literal: true
 
-class Tutor::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
-  layout "login"
+class Tutors::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  def new
-    self.resource = resource_class.new(sign_in_params)
-  end
+  # def new
+  #   super
+  # end
 
   # POST /resource/sign_in
   # def create
-  #   # super
+  #   super
   # end
 
   # DELETE /resource/sign_out
@@ -19,11 +18,7 @@ class Tutor::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
-
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
-  end
+  # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
